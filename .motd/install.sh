5381fc714a621cd05c43d52bb74200436ff284f3
McTRASH692 <rgardnerthompson@gmail.com>
2025-09-18 10:37:45 +1000

    fixed .motd

---
+++ .motd/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/Generator/termux-motd.git $HOME/.motd
+
+[ $(basename $SHELL) == "zsh" ] && _PROFILE=$HOME/.zprofile || _PROFILE=$HOME/.profile
+[ $(grep "$HOME/.motd/init.sh" $_PROFILE) != "0" ] && echo "$HOME/.motd/init.sh" >> $_PROFILE
